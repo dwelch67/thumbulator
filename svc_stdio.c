d96c552cc1075ca03c17fa9a78d9b53e2ffd71be
Robert Sexton <robert@kudra.com>
2020-09-28 11:55:07 -0500

    Baseline stdio-based system calls.   Not yet known good.

--- svc_stdio.c
+++ svc_stdio.c
@@ -11,6 +11,8 @@
 // Access ram via the official paths.
 extern uint32_t fetch32 ( uint32_t addr );
 extern uint32_t fetch16 ( uint32_t addr );
+extern void write32 ( uint32_t addr, uint32_t data );
+
 
 void svc_init() { 
   fprintf(stderr,"SVC Layer Initialization\n");
@@ -25,23 +27,62 @@ void svc_handler(uint32_t addr) {
 
 
   // printf("SVC addr %x pc %0x\n", addr, pc);
-  fprintf(stderr,"SVC %d\n", syscall_no);
+  // fprintf(stderr,"SVC %d\n", syscall_no);
   
   // There are 8 items on the stack.   Display them.
 
-  fprintf(stderr,"%08x: ", addr);
+  // fprintf(stderr,"%08x: ", addr);
 
-  for ( int i = addr; i < (addr + 0x20); i = i + 4) {
-    fprintf(stderr,"%08x ", fetch32(i));
-  }
-  fprintf(stderr,"\n");
+  // for ( int i = addr; i < (addr + 0x20); i = i + 4) {
+  //  fprintf(stderr,"%08x ", fetch32(i));
+  //}
+  //fprintf(stderr,"\n");
 
 
   switch(syscall_no) {
-    case 2:
-    default:
+    case 2: // The chars avail call.  Arg: Stream Number
+      {
+        int ret = getc(stdin);
+        if ( ret == EOF ) {
+          write32(addr, 0);
+          fprintf(stderr, "scv/swi GetC EOF\n");
+          return;
+          }
+        // Otherwise, push it back.
+        ungetc( ret, stdin);
+        write32(addr, 1);
+        return;
+      }
+      break; 
       
-      putchar(fetch16(addr+4) & 0xFF );
+    case 3: // Getchar.   Arg: Stream Number
+      {
+        int ret = getchar();
+        if ( ret == EOF ) {
+          write32(addr, -1);
+          return;  
+          } 
+        // Write the data into the stack.
+        fprintf(stderr, "scv/swi Rx Character: 0x%x / %c \n", ret, ret);
+        write32(addr, ret);
+        return;
+      }
+      
+    case 5: 
+      // Putchar: Arg: Data, Stream Number
+      {
+        uint8_t c = fetch16(addr) & 0xFF; 
+        putchar(c);
+        fprintf(stderr, "scv/swi Tx Character: 0x%02x / %c\n", c,c);
+
+        // Set the return code as zero.
+        write32(addr, 0);
+        return;        
+      }
+    default:
+      fprintf(stderr, "---------- Other svc/swi %d ---------------\n", syscall_no);
+        // Everything else does nothing...
+        break;
   }
 
   return;
